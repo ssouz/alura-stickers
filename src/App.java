d6f255f28da54a7916f3a1962f684c81713dd4f0
souz <you@example.com>
2022-07-18 10:38:50 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,24 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // conexao http e buscar lista do api
+        String url = "https://imdb-api.com/en/API/Top250Movies/k_w6e6zmqw";
+        URI endereço = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereço).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
+
+
+        // extrair dados [titulo,postere e classificar]
+
+        // exibir e manipular os dados
+
+    }
+}
