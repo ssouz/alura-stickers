e3792eaf6fb881d89ee0e139a37fc52ade299e18
souz <you@example.com>
2022-07-18 17:04:52 -0300

    finalizada a primeira aula

--- src/App.java
+++ src/App.java
@@ -3,22 +3,32 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
         // conexao http e buscar lista do api
-        String url = "https://imdb-api.com/en/API/Top250Movies/k_w6e6zmqw";
+        String url = "https://api.mocki.io/v2/549a5d8b";
         URI endereço = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereço).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
-
+        
+        
 
         // extrair dados [titulo,postere e classificar]
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
-        // exibir e manipular os dados
+        // exibir os dados
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+        }
+
 
     }
 }
